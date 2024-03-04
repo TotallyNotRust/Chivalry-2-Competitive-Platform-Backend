962e8406696830c4c907229861b9ac35dcec99c1
Gustav Thoft <35370673+TotallyNotRust@users.noreply.github.com>
2024-03-04 19:14:51 +0100

    removed matchmaking, as its handled by another api now

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use actix_web::{web, App, HttpServer};
 use diesel::{Connection, MysqlConnection};
 use dotenv::dotenv;
 use lib::database::model::Account;
-use routes::matchmake::matchmake;
 use routes::ping::ping;
 use routes::{account::account_info, login::login, signup::signup};
 use std::env;
